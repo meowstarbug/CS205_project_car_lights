4b955683e1ffa7f2689fd31d187b5454d2b3be46
Meow_star_bug <45825650+meowstarbug@users.noreply.github.com>
2019-12-24 02:00:12 +0800

    写了一大堆没测的玩意

---
+++ digital_design_project.srcs/sources_1/new/controls_led_buzz.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+
+module d_light(d, err, out);
+    input [0:0] d, err;
+    output [0:0] out;
+    err_led nkn(.led(d), .err(err), .actual_led(out));
+endmodule
+
+module l_light(clk_3_ro, l, e, err, out);
+// works for both left and right lights
+    input [2:0] clk_3_ro;
+    input [0:0] l, e, err;
+    output [2:0] out;
+    
+    wire [2:0] l_light_wo_err;
+    wire [0:0] en;
+
+    assign en = l|e;
+    assign l_light_wo_err = (en==1'b1)?clk_3_ro:3'b000;
+
+    err_led nkn(.led(l_light_wo_err), .err(err), .actual_led(out));
+endmodule
+
+module p_light(b, p, blink_clk, out);
+    input [0:0] b, p, blink_clk;
+    output reg [0:0] out;
+
+    always @(*)
+    begin
+        case ({b, p})
+            00: out <= 1'b0;
+            10: out <= blink_clk;
+            01: out <= 1'b1;
+            11: out <= 1'b1;
+            default: out <= 1'b0;
+        endcase
+    end
+endmodule
+
+module buzzer_control(buzz_clk, l, r, e, out);
+    input [0:0] buzz_clk;
+    input [0:0] l, r, e;
+    output [0:0] out;
+    wire [0:0] en;
+
+    assign en = l|r|e;
+    assign out = (en==1)?buzz_clk:1'b0;
+
+endmodule // buzzer_control
--- digital_design_project.srcs/sources_1/new/err_detection.v
+++ digital_design_project.srcs/sources_1/new/err_detection.v
@@ -16,3 +16,10 @@ module err_led(led, err, actual_led);
     output [0:0] actual_led;
     assign actual_led = led&(~err);
 endmodule
+
+module err_led_3(led, err, actual_led);
+    input [0:0] err;
+    input [2:0] led;
+    output [2:0] actual_led;
+    assign actual_led = (err==0)?led:3'b000;
+endmodule
